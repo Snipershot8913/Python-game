e536c48b955955e86ee75dad2daa93ede69e394f
Snipershot8913 <153301576+Snipershot8913@users.noreply.github.com>
2025-02-07 09:57:14 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import math 
+import random 
+pygame.init()
+screen = pygame.display.set_mode((1000,500),pygame.RESIZABLE)
+clock = pygame.time.Clock()
+running = True
+#def var and funtions
+
+
+#main run
+while running:
+  for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+  listrun=0
+  if keys[pygame.K_w]:
+
+  if keys[pygame.K_s]:
+
+  if keys[pygame.K_a]:
+
+  if keys[pygame.K_d]:
+
+  
+  
+
